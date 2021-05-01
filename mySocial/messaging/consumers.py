237d52442156768afaaadcc1cf9018b1f1f402d6
Soufiane-Fartit <soufiane.fartit@gmail.com>
2021-05-01 18:38:50 +0200

    working on video chat

--- mySocial/messaging/consumers.py
+++ mySocial/messaging/consumers.py
@@ -1,4 +1,5 @@
 import json
+from flask import request
 from flask_socketio import emit
 from flask_login import current_user
 from .. import socketio
@@ -20,3 +21,35 @@ def videocall(message):
         {'msg': {'user': {c.name: getattr(current_user, c.name) for c in current_user.__table__.columns}, 'convId':message['convId']}}, 
         broadcast=True, 
         include_self=False)
+
+
+
+# WEBRTC
+
+connectedUsers = []
+@socketio.on('connect')
+def onConnect():
+    global connectedUsers
+    connectedUsers.append(request.sid)
+
+    otherUsers = [x for x in connectedUsers if x!= request.sid]
+
+    emit('other-users', otherUsers)
+
+@socketio.on('offer')
+def onOffer(socketId, description):
+    emit('offer', (request.sid, description), room=socketId)
+
+@socketio.on('answer')
+def onAnswer(socketId, description):
+    emit('answer', description, room=socketId)
+
+@socketio.on('candidate')
+def onCandidate(socketId, candidate):
+    emit('candidate', candidate, room=socketId)
+
+@socketio.on('disconnect')
+def onDisconnect():
+    global connectedUsers
+    connectedUsers = [x for x in connectedUsers if x!=request.sid]
+
